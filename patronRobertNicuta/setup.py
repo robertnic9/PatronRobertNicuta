a277eb5ab26f49e22da4717930154b064beac35c
robertnic9 <160528408+robertnic9@users.noreply.github.com>
2024-02-19 20:07:46 +0100

    Add files via upload

---
+++ patronRobertNicuta/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PatronRobertNicuta',
+    version='0.1',
+    packages=find_packages(),
+    description='Animacion Para Expo 2024 Ibiza',
+    long_description=open('README.md').read(),
+    author='Robert Nicuta',
+    author_email='robertnicuta80@gmail.com',
+    url='https://github.com/tu_usuario/nombre_paquete',
+    instal_requires=[],
+)
